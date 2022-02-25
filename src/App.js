f4ff7f21cec8a8f22c0f7dccff03e630cb30af9b
Mercedes <mercedes.fresno@gmail.com>
2022-02-25 20:30:07 -0300

    agrego contenido clase react3

--- src/App.js
+++ src/App.js
@@ -20,3 +20,22 @@ const App =()=>{
 }
 
 export default App;
+
+
+//DESESTRUCTURACION JS
+
+// //Objeto 
+// const persona ={
+//   nombre: "jacques"
+//   apellido: "darrida"
+//   profesion: "psicologo"
+
+// }
+// //Desestructuracion 
+//  const {nombre, profesion}= persona 
+
+//  // es equivalente  en Js a 
+//  const nombre = persona.nombre
+//  const profesion= persona.profesion 
+
+
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,26 @@
 
 import "./Button.css"
-const Button =(props)=>{
-    console.log (props)
-    return (
-        <button 
-        className= {`boton ${props.color}`}
-        id="1">
-        {props.mensaje}
-        </button>
-    )
+// const Button =(props)=>{
+//     console.log (props)
+//     return (
+//         <button 
+//         className= {`boton ${props.color}`}
+//         id="1">
+//         {props.mensaje}
+//         </button>
+//     )    
+//  }
+
+
+ //Desestructurar las props 
+ const Button= ({color, mensaje})=>{
+ return (
+     <button 
+     className= {`boton ${color}`}
+     id="1">
+     {mensaje}
+     </button>
+ )
 }
 export default Button;
 
